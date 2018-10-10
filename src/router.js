14bdb03f9169fbe4b462b2db1b66de040ba266c3
Matt Bell <mappum@gmail.com>
2018-10-10 17:49:39 +0900

    Fixed existing state getting overridden by default route substate

--- src/router.js
+++ src/router.js
@@ -75,7 +75,7 @@ function Router (routes) {
   // lotion initialization handler
   function initializer (state, chain) {
     for (let route in routes) {
-      let substate //= state[route]
+      let substate = state[route]
 
       if (routes[route].initialState != null) {
         if (route in state) {
